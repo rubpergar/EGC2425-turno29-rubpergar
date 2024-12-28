c186848c7b1215614ee99b4b94bd622b29de6823
Rubén Pérez Garrido <rupg29@gmail.com>
2024-12-28 17:52:08 +0100

    Cambio con conflicto rama rq2

--- app/modules/profile/models.py
+++ app/modules/profile/models.py
@@ -5,7 +5,7 @@ class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
 
-    orcid = db.Column(db.String(19))
+    orcid = db.Column(db.String(19)) #Conflicto rq2
     affiliation = db.Column(db.String(100))
     name = db.Column(db.String(100), nullable=False)
     surname = db.Column(db.String(100), nullable=False)
